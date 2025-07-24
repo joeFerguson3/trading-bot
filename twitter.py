33b203b257d03b2469f069f17003530bc4ec620b
Joseph Ferguson <jjferguson1@sheffield.ac.uk>
2025-07-24 15:54:29 +0100

    updated get latest tweets

--- twitter.py
+++ twitter.py
@@ -1,22 +1,76 @@
-import os
 import requests
+import time
+from datetime import datetime, timedelta, timezone
 
-# Gets most recent tweets from users in the user list
-TWITTER_API = ''
+# Configuration
+API_KEY = ""
+TARGET_ACCOUNT = "BBCNews"  # The account to monitor
+CHECK_INTERVAL = 300 # Time between each check
+LAST_CHECKED_TIME = datetime.now(timezone.utc) - timedelta(hours=1) # Sets last checked time to 1hr ago
 
-# Username to get tweets from
-username = 'cnnbrk'
+def new_tweets():
+    global LAST_CHECKED_TIME
+    
+    # Format times for the API query
+    until_time = datetime.now(timezone.utc)
+    since_time = LAST_CHECKED_TIME
+    
+    # Format times as strings in the format API expects
+    since_str = since_time.strftime("%Y-%m-%d_%H:%M:%S_UTC")
+    until_str = until_time.strftime("%Y-%m-%d_%H:%M:%S_UTC")
+    
+    # Construct the query
+    query = f"from:{TARGET_ACCOUNT} since:{since_str} until:{until_str} include:nativeretweets"
+    
+    # API endpoint
+    url = "https://api.twitterapi.io/twitter/tweet/advanced_search"
+    
+    # Request parameters
+    params = {
+        "query": query,
+        "queryType": "Latest"
+    }
+    
+    # Headers with API key
+    headers = {
+        "X-API-Key": API_KEY
+    }
+    
+    all_tweets = []
 
-# Url to the users tweets
-url = f'https://api.twitterapi.io/twitter/user/last_tweets?userName=cnnbrk'
+    response = requests.get(url, headers=headers, params=params)
+    
+    # Parse the response
+    if response.status_code == 200:
+        data = response.json()
+        tweets = data.get("tweets", [])
+        
+        if tweets:
+            all_tweets.extend(tweets)
+            
 
-headers = {'X-API-Key': TWITTER_API}
+    # Process all collected tweets
+    if all_tweets:
+        print(f"Found {len(all_tweets)} total tweets from {TARGET_ACCOUNT}!")
+        for tweet in all_tweets:
+            print(f"{tweet['text']}")
+            # TODO Analyse tweets
+    else:
+        print(f"No new tweets from {TARGET_ACCOUNT} since last check.")
+    
+    # Update the last checked time
+    LAST_CHECKED_TIME = until_time
 
-# Requests API
-response = requests.get(url, headers=headers)
-print(response)
+# Main monitoring loop
+def main():
+    print(f"Starting to monitor tweets from @{TARGET_ACCOUNT}")
+    print(f"Checking every {CHECK_INTERVAL} seconds")
+    
+    try:
+        while True:
+            new_tweets()
+            time.sleep(CHECK_INTERVAL)
+    except KeyboardInterrupt:
+        print("Monitoring stopped.")
 
-tweets = response.json().get("data", {}).get("tweets", [])
-
-for tweet in tweets:
-    print(tweet.get("text", "No text"))
+main()
