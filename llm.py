022a33f53d8253bec509cc202f30e7b89394702d
Joseph Ferguson <jjferguson1@sheffield.ac.uk>
2025-07-27 16:56:38 +0100

    stock analysis

--- llm.py
+++ llm.py
@@ -1,10 +1,11 @@
 import ollama
 
-response = ollama.chat(
-    model='gemma3',
-    messages=[
-        {"role": "user", "content": "hello."}
-    ]
-)
+
+messages = [
+    {"role": "system", "content": "You are a financial analysis assistant. Your task is to analyze text and assess confidence in a specific stock's performance. Based on the content, provide a score from 0 to 100, where:\n\n- 0 = Strong Sell\n- 50 = Hold\n- 100 = Strong Buy\n\nInclude a brief explanation of your reasoning. Output format:\n\nIn-depth Analysis: [Your explanation]\n[Stock Name]: [Score]"},
+    {"role": "user", "content": "test"}
+]
+
+response = ollama.chat(model='gemma3', messages=messages)
 
 print(response['message']['content'])
